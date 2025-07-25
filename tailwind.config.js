a2f8b076049c71b94814212c38a5ffc222f6b96a
kazuhaaaaan <nouvaprasetyaa@gmail.com>
2025-07-26 01:26:18 +0700

    baru

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#6366f1',
+        secondary: '#0ea5e9',
+        dark: '#334155',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
